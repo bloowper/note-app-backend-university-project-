e895d49dd2f099213bb4fe4303bc361a70368a0c
tomek <gtatomek@gmail.com>
2022-02-01 01:00:36 +0100

    adding dto for controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,12 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 import { Task } from "./task.model";
+import { CreateTaskDto } from "./dto/create-task.dto";
 
 @Controller("tasks")
 export class TasksController {
-  constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService) {
+  }
 
   @Get()
   getAllTasks(): Task[] {
@@ -12,10 +14,7 @@ export class TasksController {
   }
 
   @Post()
-  createTask(
-    @Body("title") title: string,
-    @Body("description") description: string
-  ): Task {
-    return this.tasksService.createTask(title, description);
+  createTask(@Body() taskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(taskDto);
   }
 }
