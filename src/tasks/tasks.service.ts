996fb98bd5f5328455ec6433ac10f6e2cc90c03e
tomek <gtatomek@gmail.com>
2022-02-02 00:44:23 +0100

    get task by id

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,13 +12,20 @@ export class TasksService {
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
+    //restructuring syntax
+    //Destructuring assignment
+    const { title, description } = createTaskDto;
     const task: Task = {
       id: uuidv4(),
-      title: createTaskDto.title,
-      description: createTaskDto.description,
-      status: TaskStatus.IN_PROGRESS
+      title,
+      description,
+      status: TaskStatus.IN_PROGRESS,
     };
     this.tasks.push(task);
     return task;
+  }
+
+  getTaskById(id: string): Task | undefined {
+    return this.tasks.find((task) => task.id === id);
   }
 }
