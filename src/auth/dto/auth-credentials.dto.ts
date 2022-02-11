6353a773c767e35e8c5d8bc7d9ac21a02039434c
tomek <gtatomek@gmail.com>
2022-02-12 00:34:41 +0100

    password validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,22 @@
+import {
+  IsString,
+  isString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from "class-validator";
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(64)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: "password is to weak"
+  })
   password: string;
 }
