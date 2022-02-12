7e395b3d9e1aa1cc675a0fd3f46c005fbae1433e
tomek <gtatomek@gmail.com>
2022-02-12 22:29:25 +0100

    adding passwort

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { UsersRepository } from "./users.repository";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { compare } from "bcrypt";
 
 @Injectable()
 export class AuthService {
@@ -8,5 +9,15 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.usersRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const userEntity = await this.usersRepository.findOne({ username});
+    if (userEntity && (await compare(password, userEntity.password))) {
+      return "success";
+    } else {
+      throw new UnauthorizedException("Credentials incorrect");
+    }
   }
 }
